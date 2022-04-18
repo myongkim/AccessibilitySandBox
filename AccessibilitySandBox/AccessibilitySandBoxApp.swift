e5be25ff5c48e51d27c69ae5a868082690b49209
Isaac Kim <myong328@gmail.com>
2022-04-18 13:37:46 -0700

    Initial Commit

---
+++ AccessibilitySandBox/AccessibilitySandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandBoxApp.swift
+//  AccessibilitySandBox
+//
+//  Created by Isaac Kim on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
